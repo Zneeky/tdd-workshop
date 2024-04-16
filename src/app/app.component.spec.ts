f6bcbc640c3a4a7f1526d62af2a3956efd2cfdf0
Arkan Ahmedov <arkanahmedov02@gmail.com>
2024-04-16 17:20:08 +0300

    test(component):adding checks for inital test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,4 +14,13 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
+  it(`shouuld have a number property, which the is bout to`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    fixture.detectChanges();
+    expect(app.number).toEqual(0);
+    const input = fixture.nativeElement.querySelector('.test-class');
+    expect(input.value).toEqual('');
+  })
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IGX_INPUT_GROUP_DIRECTIVES, IgxButtonDirective, IgxIconModule, IgxInputGroupModule } from 'igniteui-angular';
+import { IgxInputGroupBase } from 'igniteui-angular/lib/input-group/input-group.common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IGX_INPUT_GROUP_DIRECTIVES, IgxButtonDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+public number : number | undefined = undefined;
 }
